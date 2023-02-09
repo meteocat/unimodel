761c512b57f2a065a150bb256de6c963ce3e065e
ecasellas <enric.casellas@gmail.com>
2023-02-09 07:59:18 +0100

    PME-15 Afegim la lectura del Moloch i tests

--- importers_nwp.py
+++ importers_nwp.py
@@ -1,10 +1,14 @@
+"""Module to read NWP files and transfrom them to xarray.
+"""
+import numpy as np
 import pyproj
 import xarray
 from rasterio import Affine
 
+from unimodel.utils.geotools import proj4_from_grib
 
-def _get_wrf_prs_metadata(xarray_var):
 
+def _get_wrf_prs_metadata(xarray_var: xarray.DataArray) -> dict:
     """Get projection, Affine transform and shape from a PRS xarray.
 
     Args:
@@ -13,7 +17,6 @@ def _get_wrf_prs_metadata(xarray_var):
     Returns:
         dict: CRS, x size, y size and Affine transform.
     """
-
     dx = xarray_var.attrs['GRIB_DxInMetres']
     dy = xarray_var.attrs['GRIB_DyInMetres']
     nx = xarray_var.attrs['GRIB_Nx']
@@ -49,22 +52,76 @@ def _get_wrf_prs_metadata(xarray_var):
             'crs': crs_wrf, 'x_size': nx, 'y_size': ny}
 
 
-def read_wrf_prs(file,variable):
-    """Read wrf variable chosen in a wrf-PRS grib file
+def read_wrf_prs(grib_file: str, variable: str) -> xarray.DataArray:
+    """Reads a WRF grib file and transforms it into an xarray.DataArray.
 
     Args:
-        file (string): Path of the file which has to be readed
-        variable (string): Variable to extract
+        grib_file (string): Path to a WRF grib file.
+        variable (string): Variable to extract.
     Returns:
-        xarray: Contains the data and the geographical information to  
+        xarray: Contains the data and the geographical information to
                 transform the grids
     """
+    grib_data = xarray.open_dataarray(grib_file, engine='cfgrib',
+                backend_kwargs=dict(filter_by_keys={'shortName': variable}))
 
-    ds_data = xarray.open_dataarray(file, engine='cfgrib',
-                    backend_kwargs=dict(filter_by_keys={'shortName': variable}))
- 
-    geographics = _get_wrf_prs_metadata(ds_data)
-    ds_data = ds_data.rio.write_crs(geographics['crs'])
-    ds_data.rio.write_transform(geographics['affine'], inplace=True)
+    geographics = _get_wrf_prs_metadata(grib_data)
+    grib_data = grib_data.rio.write_crs(geographics['crs'])
+    grib_data.rio.write_transform(geographics['affine'], inplace=True)
 
-    return ds_data
+    return grib_data
+
+
+def read_moloch_grib(grib_file: str, variable: str) -> xarray.DataArray:
+    """Reads a Moloch grib file and transforms it into an xarray.DataArray.
+
+    Args:
+        grib_file (str): Path to a Moloch grib file.
+        variable (str): Variable to extract.
+
+    Returns:
+        xarray: Moloch grib file data.
+    """
+    grib_filter = {'shortName': variable}
+
+    grib_data = xarray.open_dataarray(grib_file, engine='cfgrib',
+                    backend_kwargs=dict(filter_by_keys=grib_filter))
+
+    grib_md = _get_moloch_metadata(grib_data)
+
+    grib_data.rio.write_crs(grib_md['crs'], inplace=True)
+
+    x_coords = np.linspace(grib_md['affine'].c,
+                           grib_md['affine'].c + (grib_md['x_size'] *
+                           grib_md['affine'].a) - grib_md['affine'].a,
+                           grib_md['x_size'])
+
+    y_coords = np.linspace(grib_md['affine'].f,
+                            grib_md['affine'].f + (grib_md['y_size'] *
+                            grib_md['affine'].e) - grib_md['affine'].e,
+                            grib_md['y_size'])
+
+    grib_data = grib_data.assign_coords(x=x_coords, y=y_coords)
+    grib_data.rio.write_crs(grib_md['crs'], inplace=True)
+    grib_data = grib_data.drop_vars(['latitude', 'longitude'], errors='ignore')
+
+    grib_data.rio.write_transform(grib_md['affine'], inplace=True)
+
+    return grib_data
+
+
+def _get_moloch_metadata(moloch_data: xarray.DataArray) -> dict:
+
+    moloch_projection = proj4_from_grib(moloch_data)
+
+    moloch_extension = Affine.from_gdal(
+        moloch_data.attrs['GRIB_longitudeOfFirstGridPointInDegrees'],
+        moloch_data.attrs['GRIB_iDirectionIncrementInDegrees'],
+        0,
+        moloch_data.attrs['GRIB_latitudeOfFirstGridPointInDegrees'],
+        0,
+        moloch_data.attrs['GRIB_jDirectionIncrementInDegrees'])
+
+    return {'affine': moloch_extension, 'crs': moloch_projection,
+            'x_size': moloch_data.attrs['GRIB_Ny'],
+            'y_size': moloch_data.attrs['GRIB_Nx']}
