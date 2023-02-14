e40f0064f7201ffbbbf16f0ed6704b9e61a25a53
enricasellas <enric.casellas@gencat.cat>
2023-02-14 08:21:00 +0100

    PME-18 Canvi de documentació PR

--- unimodel/io/interface.py
+++ unimodel/io/interface.py
@@ -1,20 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-pysteps.motion.interface
-========================
-Interface for the motion module. It returns a callable optical flow routine for
-computing the motion field.
-The methods in the motion module implement the following interface:
-    ``motion_method(precip, **keywords)``
-where precip is a (T,m,n) array containing a sequence of T two-dimensional input
-images of shape (m,n). The first dimension represents the images time dimension
-and the value of T depends on the type of the method.
-The output is a three-dimensional array (2,m,n) containing the dense x- and
-y-components of the motion field in units of pixels / timestep as given by the
-input array R.
-.. autosummary::
-    :toctree: ../generated/
-    get_method
+"""Interface for I/O grib readers.
 """
 from unimodel.io.readers_nwp import (read_arome_grib, read_arpege_grib,
                                      read_bolam_grib, read_icon_grib,
@@ -30,10 +14,18 @@ _readers['wrf'] = read_wrf_prs
 
 
 def get_reader(name):
-    """
-    Return a callable function for the reader method corresponding to
+    """Returns a callable function for the reader method corresponding to
     the given name. The available options are 'arome', 'arpege', 'bolam',
     'icon', 'moloch' and 'wrf'.
+
+    Args:
+        name (str): Name of the NWP model.
+
+    Raises:
+        ValueError: If 'name' not in the available model list.
+
+    Returns:
+        function: NWP grib reader.
     """
     if isinstance(name, str):
         name = name.lower()
@@ -42,7 +34,5 @@ def get_reader(name):
         reader_method = _readers[name]
         return reader_method
     except KeyError:
-        raise ValueError(
-            f"Unknown reader {name}\n The available readers are: "
-            + str(list(_readers.keys()))
-        ) from None
+        raise ValueError(f"Unknown reader {name}\n The available readers are: "
+                         + str(list(_readers.keys()))) from None
