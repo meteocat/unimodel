014b1f96f1f1deb480da1f65ab40f7d42ea5c23b
ecasellas <enric.casellas@gmail.com>
2023-02-22 08:18:10 +0100

    Es refà la funció hourly_accumulations i documentació exporters

--- tests/test_exporters.py
+++ tests/test_exporters.py
@@ -1,4 +1,4 @@
-"""Tests of constructing a netcdf from an xarray list
+"""Tests building a netcdf from an xarray.
 """
 import pickle
 import unittest
@@ -8,22 +8,22 @@ import xarray
 from unimodel.io.exporters import export_to_netcdf
 
 
-class TestExportersNWP(unittest.TestCase):
-    """Tests list array to netcdf."""
-    def test_exporters_to_netcdf(self):
+class TestExporters(unittest.TestCase):
+    """Tests exporters from xarray to netcdf."""
 
-        variable='tp'
-        netcdf_file='tests/data/out/reproject_models.nc'
+    def test_export_to_netcdf(self):
+        """Test export xarray to netcdf."""
+        netcdf_file = 'tests/data/out/reproject_models.nc'
 
         with open('tests/data/list_arome_xarray.pkl', 'rb') as file:
-            list_arome_xarrays = pickle.load(file)
+            arome_xarrays = pickle.load(file)
         with open('tests/data/list_bolam_xarray.pkl', 'rb') as file:
-            list_bolam_xarrays = pickle.load(file)
-    
-        list_models_arrays=[list_arome_xarrays, list_bolam_xarrays]
-        export_to_netcdf(netcdf_file, list_models_arrays, var=variable)
+            bolam_xarrays = pickle.load(file)
 
-        netcdf_data= xarray.open_dataset(netcdf_file)
+        models_arrays = [arome_xarrays, bolam_xarrays]
+        export_to_netcdf(models_arrays, netcdf_file)
+
+        netcdf_data = xarray.open_dataset(netcdf_file)
 
         self.assertEqual(netcdf_data.dims['x'], 620)
         self.assertEqual(netcdf_data.dims['y'], 417)
@@ -31,18 +31,12 @@ class TestExportersNWP(unittest.TestCase):
         self.assertEqual(netcdf_data.dims['model'], 2)
 
         self.assertAlmostEqual(netcdf_data.rio.transform().a,
-                               0.010642497622783) 
-        self.assertAlmostEqual(netcdf_data.rio.transform().b, 
-                               0.0)
+                               0.010642497622783)
+        self.assertAlmostEqual(netcdf_data.rio.transform().b, 0.0)
         self.assertAlmostEqual(netcdf_data.rio.transform().c,
-                               -1.621137007661705) 
-        self.assertAlmostEqual(netcdf_data.rio.transform().d, 
-                               0.0)
-        self.assertAlmostEqual(netcdf_data.rio.transform().e, 
+                               -1.621137007661705)
+        self.assertAlmostEqual(netcdf_data.rio.transform().d, 0.0)
+        self.assertAlmostEqual(netcdf_data.rio.transform().e,
                                -0.010642497622783)
-        self.assertAlmostEqual(netcdf_data.rio.transform().f, 
+        self.assertAlmostEqual(netcdf_data.rio.transform().f,
                                43.455589042260094)
-
-
-
-        
