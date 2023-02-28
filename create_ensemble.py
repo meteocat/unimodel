54ddc3615fe0a933bd6e9b342d59048ccb5ad5d0
ecasellas <enric.casellas@gmail.com>
2023-02-28 08:37:26 +0100

    PME-28 Reorganització i documentació script principal

--- create_ensemble.py
+++ create_ensemble.py
@@ -1,16 +1,30 @@
-import unimodel.io
+"""Main script to create an ensemble from different NWP model gribs.
+"""
 from datetime import datetime
-from unimodel.io.importers_nwp import import_nwp_grib
-from unimodel.downscaling.interpolation import bilinear
-from unimodel.io.exporters import export_to_netcdf
-from unimodel.utils.load_config import load_config
+
 import dask
 
+import unimodel.io
+from unimodel.downscaling.interpolation import bilinear
+from unimodel.io.exporters import export_to_netcdf
+from unimodel.io.importers_nwp import import_nwp_grib
+from unimodel.utils.load_config import load_config
 
-def process_model(model, date, config):
+
+def __process_model(model, date, config):
+    """Processes and interpolates each model to a common grid.
+
+    Args:
+        model (str): NWP model name.
+        date (datetime): Run date of NWP model.
+        config (dict): Configuration dictionary
+
+    Returns:
+        list: xarray including all lead times of NWP model run.
+    """
     out_model = []
-    for lt in range(config['lead_times']):
-        nwp_file = import_nwp_grib(date, lt, model, config)
+    for lead_time in range(config['lead_times']):
+        nwp_file = import_nwp_grib(date, lead_time, model, config)
         reader = unimodel.io.get_reader(model)
         nwp_data = reader(nwp_file, 'tp', model)
         rep_data = bilinear(nwp_data, config['corner_ul'],
@@ -21,32 +35,39 @@ def process_model(model, date, config):
             if 'GRIB' in attributes:
                 del rep_data.attrs[attributes]
         out_model.append(rep_data)
-    
+
     return out_model
 
-# Definim variables a partir del fitxer de configuracio
-# config = load_config(ARGS.config)
-config = load_config('config_unimodel.json')
 
-models = ['wrf_gfs_9', 'wrf_gfs_3', 'wrf_exp', 'wrf_ecm',
-          'moloch_ecm', 'moloch_gfs', 'ecmwf_hres', 'arome',
-          'bolam', 'icon', 'arpege']
+def main():
+    """Creates an ensemble from different NWP model grib files.
+    """
+    # Definim variables a partir del fitxer de configuracio
+    # config = load_config(ARGS.config)
+    config = load_config('config_unimodel.json')
 
-models = ['moloch_ecm']
+    models = ['wrf_gfs_9', 'wrf_gfs_3', 'wrf_exp', 'wrf_ecm',
+              'moloch_ecm', 'moloch_gfs', 'ecmwf_hres', 'arome',
+              'bolam', 'icon', 'arpege']
 
-date = datetime(2023, 2, 24)
-list_out_models=[]
+    models = ['moloch_ecm']
 
-time_total_0 = datetime.utcnow()
-lazy_results = []
-for model in models:
-    lazy_result = dask.delayed(process_model)(model, date, config)
-    lazy_results.append(lazy_result)
+    date = datetime(2023, 2, 24)
 
-processed = dask.compute(*lazy_results)
+    time_total_0 = datetime.utcnow()
+    lazy_results = []
+    for model in models:
+        lazy_result = dask.delayed(__process_model)(model, date, config)
+        lazy_results.append(lazy_result)
 
-export_to_netcdf(processed, config['netcdf_output'])
+    processed = dask.compute(*lazy_results)
+
+    export_to_netcdf(processed, config['netcdf_output'])
+
+    time_total_1 = datetime.utcnow()
+    print((time_total_1 - time_total_0).total_seconds() / 60)
 
 
-time_total_1 = datetime.utcnow()
-print((time_total_1 - time_total_0).total_seconds() / 60)
+if __name__ == '__main__':
+
+    main()
