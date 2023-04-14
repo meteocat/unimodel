b2b5095062b5ef3845e92de016b5cacf4e8ddf97
iciar <iciarguerrero@gencat.cat>
2023-04-14 11:43:40 +0200

    function for calculating neighbours

--- unimodel/downscaling/ecorrection.py
+++ unimodel/downscaling/ecorrection.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+import xarray as xr
+from sklearn.neighbors import NearestNeighbors
 
 class Ecorrection():
 
@@ -21,4 +24,31 @@ class Ecorrection():
             raise FileNotFoundError(f'{config["hres_dem_file"]} not found')
         
         self.result = None
+
+    def __get_neighbours(land_binary_mask: xr.Dataset, out_file: str, neighbours: int):
+
+        try:
+
+            if not os.path.exists(out_file):
+
+                neigh_candidates = np.where(land_binary_mask == 1)
+                neigh_candidates = np.vstack((neigh_candidates[0], neigh_candidates[1])).T
+                neigh_needed = np.where(land_binary_mask >= 0)
+                neigh_needed = np.vstack((neigh_needed[0], neigh_needed[1])).T
+
+                nbrs = NearestNeighbors(n_neighbors=neighbours, algorithm='ball_tree').fit(neigh_candidates)
+                _, indices = nbrs.kneighbors(neigh_needed)
+
+                np.savez_compressed(out_file,
+                                    indices=indices,
+                                    neigh_needed=neigh_needed,
+                                    neigh_candidates=neigh_candidates)
+                
+                neigh_summary = {'indices': indices, 'neigh_neeed': neigh_needed, 'neigh_candidates': neigh_candidates}
+            
+            return neigh_summary
+            
+        except Exception:
+
+            raise NameError(f'\'land_binary_mask\' variable not defined')
 
