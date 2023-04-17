22e6febb7c9e19d667b0814e7471ec6675e8522f
iciar <iciarguerrero@gencat.cat>
2023-04-17 11:28:44 +0200

    PME-73 added test errors

--- tests/test_ecorrection.py
+++ tests/test_ecorrection.py
@@ -5,13 +5,23 @@ import unittest
 
 from unimodel.downscaling.ecorrection import Ecorrection
 
-config = {'hres_dem_file': '', 'neighbours_file': ''}
-config_wrong = {'hres_dem_file': 'ecorr/tests/data/not_found.tif'}
-
 class TestEcorrection(unittest.TestCase):
+
+    config = {'hres_dem_file': 'tests/data/test_data/hres_dummy.txt', 'neighbours_file': 'tests/data/test_data/neighbours_dummy.txt'}
+    config_wrong = {'hres_dem_file': 'ecorr/tests/data/not_found.tif'}
 
     def test_init(self):
 
-        Ecorrection('2t', config)
+        # ecor = Ecorrection('2t', self.config)
 
+        with self.assertRaises(KeyError) as err:
 
+            Ecorrection('2t', self.config_wrong)
+
+        with self.assertRaises(ValueError) as err:
+
+            Ecorrection('2d', self.config)
+
+        with self.assertRaises(FileNotFoundError) as err:
+
+            Ecorrection('2t', self.config)
