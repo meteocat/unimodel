e63361fb16e7acda1cc56023c1a93900be62c96e
iciar <iciarguerrero@gencat.cat>
2023-04-18 13:18:18 +0200

    PME-74 test_get_neighbours and error

--- tests/test_ecorrection.py
+++ tests/test_ecorrection.py
@@ -2,15 +2,20 @@
 """
 import pickle
 import unittest
+import xarray as xr
+import numpy as np
 
+from unimodel.io.readers_nwp import read_wrf_prs
 from unimodel.downscaling.ecorrection import Ecorrection
 
 class TestEcorrection(unittest.TestCase):
 
-    config = {'hres_dem_file': 'tests/data/test_data/hres_dummy.txt', 'neighbours_file': 'tests/data/test_data/neighbours_dummy.txt'}
+    config = {'hres_dem_file': 'tests/data/test_data/hres_dem_25831.tif', 
+              'neighbours_file': 'tests/data/test_data/neighbours_dummy.txt'}
     config_wrong = {'hres_dem_file': 'tests/data/test_data/hres_dummy.txt'}
-    config_nofile = {'hres_dem_file': 'tests/data/test_data/nofile.txt', 'neighbours_file': 'tests/data/test_data/nofile.txt'}
-
+    config_nofile = {'hres_dem_file': 'tests/data/test_data/nofile.txt', 
+                     'neighbours_file': 'tests/data/test_data/nofile.txt'}
+    
     def test_init(self):
 
         ecor = Ecorrection('2t', self.config)
@@ -34,7 +39,7 @@ class TestEcorrection(unittest.TestCase):
 
         self.assertEqual(err.exception.args[0],"At least 'hres_dem_file' and 'neighbours_file' must be in the config dictionary")
 
-    def test_init_file_not_found(self):
+    def test_init_hres_dem_not_found(self):
 
         with self.assertRaises(FileNotFoundError) as err:
 
@@ -42,5 +47,26 @@ class TestEcorrection(unittest.TestCase):
 
         self.assertEqual(err.exception.args[0], f'{self.config_nofile["hres_dem_file"]} not found')
    
+    def test_get_neighbours(self):
 
+        ds_lsm = read_wrf_prs('tests/data/nwp_src/wrf43_prs/WRFPRS-03.2023020600_032.grib',
+                              'lsm', 'WRF')
+        out_file = 'tests/data/test_data/neighbours.npz'
+
+        ecor = Ecorrection('2t', self.config)
+
+        neigh = ecor._Ecorrection__get_neighbours(land_binary_mask=ds_lsm, out_file=out_file, neighbours=64)
+
+    def test_get_neighbours_lsm_not_found(self):
+
+        ds_lsm_wrong = read_wrf_prs('tests/data/nwp_src/wrf43_prs/WRFPRS-03.2023020600_032.grib',
+                                    '2t', 'WRF')
+        out_file = 'tests/data/test_data/neighbours.npz'
+
+        with self.assertRaises(ValueError) as err:
+
+            ecor = Ecorrection('2t', self.config)
+            neigh = ecor._Ecorrection__get_neighbours(land_binary_mask=ds_lsm_wrong, out_file=out_file, neighbours=64)
         
+        self.assertEqual(err.exception.args[0], f'\'land_binary_mask\' variable does not exist')
+   
