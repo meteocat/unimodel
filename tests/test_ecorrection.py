7f0dd4af996b84a55b42619531b8edc278cc0686
iciar <iciarguerrero@gencat.cat>
2023-05-04 11:15:44 +0200

    PME-78 test lsm true

--- tests/test_ecorrection.py
+++ tests/test_ecorrection.py
@@ -88,6 +88,16 @@ class TestEcorrection(unittest.TestCase):
         self.assertEqual(np.round(var_correction[162,72].values, 6), 0.050095)
 
 
+    def test_apply_correction_lsm_true(self):
+            """Tests apply correction function when lsm=True
+            """
+            ecor = Ecorrection(self.da_var[0], self.dem_file)
+            var_correction = ecor.apply_correction(self.dem_file, self.da_var[1],
+                                                   self.da_var[2], landsea_mask=True)
+
+            self.assertEqual(np.round(var_correction[162,72].values, 6), 0.050095)
+
+        
     def test_apply_correction_not_2t_dataarray(self):
         """Datarray without the desired variable (2t)
         """
@@ -108,4 +118,3 @@ class TestEcorrection(unittest.TestCase):
             ecor.apply_correction(self.dem_file, self.da_var[1], self.da_var[1])
 
         self.assertEqual(err.exception.args[0], 'orography variable does not exist')
-        
